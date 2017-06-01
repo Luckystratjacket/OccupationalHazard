6a7ff927592eb67335ad3f69ec523f23e264068d
TheFunkyOwl <orangeade1999@gmail.com>
2017-06-01 14:25:34 -0400

    parking lot images
    The six (6) parking lot images were added.  These progress from normal to absolute hell.  Have fun.

--- Murder Mystery/javascript/scenes/parkingLot.js
+++ Murder Mystery/javascript/scenes/parkingLot.js
@@ -6,6 +6,7 @@ function parkingLotActions(action, state) {
                 dialog: 'You decide to remain in the parking lot. Hey, nothing bad can ever happen in a Parking Lot!',
                 choices: ['Enter the Office', 'Remain in parking lot'],
                 actions: ['officelot.ETO', 'parkingLot.RIL2'],
+                background: [{ url: './img/parkingLotNormal.png', width: '100%', height: '100%' }],
             });
 
 
@@ -15,6 +16,7 @@ function parkingLotActions(action, state) {
                 dialog: 'You continue to wait in the parking lot. Your mind drifts as you think about the bad things that could potentially happen in the lot.',
                 choices: ['Enter the Office', 'Remain in Parking Lot'],
                 actions: ['officelot.ETO', 'parkingLot.RIL3'],
+                background: [{ url: './img/parkingLot1.png', width: '100%', height: '100%' }],
             });
 
 
@@ -26,6 +28,7 @@ function parkingLotActions(action, state) {
                 dialog: 'You realise the immense doom you yourself are in for continuing to wait in the dreaded parking lot. Perhaps its time to enter the office building.',
                 choices: ['Enter the office', 'Remain in the Lot Of Doom'],
                 actions: ['officelot.ETO', 'parkingLot.RIL4'],
+                background: [{ url: './img/parkingLot1.png', width: '100%', height: '100%' }],
             });
 
 
@@ -37,6 +40,7 @@ function parkingLotActions(action, state) {
                 dialog: 'People are beginning to notice you. You\'ve been down here for 4 hours now. Perhaps you should head up to the office.',
                 choices: ['Enter the Office', 'Remain in your newfound concrete abode'],
                 actions: ['officelot.ETO', 'parkingLot.RIL5'],
+                background: [{ url: './img/parkingLot2.png', width: '100%', height: '100%' }],
             });
 
 
@@ -48,6 +52,7 @@ function parkingLotActions(action, state) {
                 dialog: 'The Concrete welcomes you. It\'s been a good 6 hours now. Concrete is warm. And happy.',
                 choices: ['Enter the office', 'Remain in the Parking Lot'],
                 actions: ['officelot.ETO', 'parkingLot.RIL6'],
+                background: [{ url: './img/parkingLot2.png', width: '100%', height: '100%' }],
             });
 
 
@@ -60,6 +65,7 @@ function parkingLotActions(action, state) {
                 dialog: 'You are one with the concrete. The narrator is getting bored with this. Perhaps you should enter the building.',
                 choices: ['Enter the office', 'Remain in the Parking Lot'],
                 actions: ['officelot.ETO', 'parkingLot.RIL7'],
+                background: [{ url: './img/parkingLot3.png', width: '100%', height: '100%' }],
             });
 
 
@@ -71,6 +77,7 @@ function parkingLotActions(action, state) {
                 dialog: 'The Parking Lot is nice. The Parking lot is good. People have tried to remove you from the lot. Those people aren\'t here anymore.',
                 choices: ['Enter the office', 'Remain in the Parking Lot'],
                 actions: ['officelot.ETO', 'parkingLot.RIL8'],
+                background: [{ url: './img/parkingLot3.png', width: '100%', height: '100%' }],
             });
 
 
@@ -82,6 +89,7 @@ function parkingLotActions(action, state) {
                 dialog: 'The concept of Time has vanished from your mind. You have ascended due to this parking lot. It blesses you for your stay. Perhaps you should enter the office now. Please.',
                 choices: ['Enter the office', 'Remain in the Parking Lot'],
                 actions: ['officelot.ETO', 'parkingLot.RIL9'],
+                background: [{ url: './img/parkingLot4.png', width: '100%', height: '100%' }],
             });
 
 
@@ -93,6 +101,7 @@ function parkingLotActions(action, state) {
                 dialog: 'The Parking Lot is your new friend. You have shared many good memories with it. Especially that time when you spent an ENTIRE MINUTE not entering the office building and instead staying in the Lot. Perhaps you should enter the Office now. What more is there in this lot?',
                 choices: ['Enter the Office', 'Remain in the Parking Lot'],
                 actions: ['officelot.ETO', 'parkingLot.RIL10'],
+                background: [{ url: './img/parkingLot5.png', width: '100%', height: '100%' }],
             });
 
 
@@ -104,6 +113,7 @@ function parkingLotActions(action, state) {
                 dialog: 'You know that the game only advances if you enter the building, right? Please enter the building. This choice Lost its charm after the first 5 turns.',
                 choices: ['Remain in the parking lot', 'Enter the office'],
                 actions: ['parkingLot.RIL11', 'officelot.ETO'],
+                background: [{ url: './img/parkingLot5.png', width: '100%', height: '100%' }],
             });
 
 
@@ -115,6 +125,7 @@ function parkingLotActions(action, state) {
                 dialog: 'Dang. I thought that would work. This is your last chance to enter the building peacefully. After that, I will have to intervine with my Narrator powers.',
                 choices: ['Enter the Office', 'Remain in the Lot'],
                 actions: ['officelot.ETO', 'parkingLot.RIL12'],
+                background: [{ url: './img/parkingLot5.png', width: '100%', height: '100%' }],
             });
 
 
@@ -126,6 +137,7 @@ function parkingLotActions(action, state) {
                 dialog: 'Ok, fine. You want to stay with your lot? FINE. Do whatever I dont care. Its your time you\'re wasting. When you\'re ready hit the button.',
                 choices: ['Remain in the lot', 'Enter the office'],
                 actions: ['parkingLot.RIL13', 'officelot.ETO'],
+                background: [{ url: './img/parkingLot5.png', width: '100%', height: '100%' }],
             });
 
 
@@ -137,6 +149,7 @@ function parkingLotActions(action, state) {
                 dialog: 'Congratu. freak\'in. Lations. This is a loop. Have fun. When you get bored just hit that convienient Enter office option.',
                 choices: ['Remain in the lot for another few days. Has it been days? I couldn\'t tell you.', 'ENTER THE OFFICE'],
                 actions: ['parkingLot.RIL13', 'officelot.ETOM'],
+                background: [{ url: './img/parkingLot5.png', width: '100%', height: '100%' }],
             });
 
 
