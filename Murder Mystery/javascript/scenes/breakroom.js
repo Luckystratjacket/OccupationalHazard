bc1af44063eb6d719a50e9acdc076843c28a41b9
Luckystratjacket <luckystratjacket1@Gmail.com>
2017-06-08 14:06:49 -0400

    Push 6/8/17
    added things

--- Murder Mystery/javascript/scenes/breakroom.js
+++ Murder Mystery/javascript/scenes/breakroom.js
@@ -1,9 +1,65 @@
 function breakroomActions(action, state) {
     var newState = state;
         switch (action) {
-        case '':  
+        case 'ent':  
                newState = Object.assign(newState, {
-                dialog: '',
+                dialog: 'You enter the breakroom. You can see Benny, John\'s son, in the corner crying.',
+                choices: ['Look around the break room for clues', 'Talk to Benny'],
+                actions: ['breakroom.inv', 'breakroom.dia'],
+            });
+            break;
+            case 'inv':  
+               newState = Object.assign(newState, {
+                dialog: 'You look around the breakroom. All you can find is an old yogurt that was suprisingly good. It had someone\'s name on it, but hey, Finders keepers.',
+                choices: ['Talk to Benny', 'Exit into the hallway'],
+                actions: ['breakroom.dia', 'hallway.ent3'],
+            });
+            break;
+            case 'dia':  
+               newState = Object.assign(newState, {
+                dialog: '[Benny] -"You never resonded to me about my question in the conference room."',
+                choices: ['Why are you crying?', 'What question?'],
+                actions: ['breakroom.dia2', 'breakroom.dia3'],
+            });
+            break;
+            case 'dia2':  
+               newState = Object.assign(newState, {
+                dialog: '[Benny] -"I dont know where my dad is and no one will tell me. Do you know where my dad is?"',
+                choices: ['No', 'Yes'],
+                actions: ['breakroom.dia4', 'breakroom.dia5'],
+            });
+            break;
+            case 'dia3':  
+               newState = Object.assign(newState, {
+                dialog: '[Benny] -"Do you know where my dad is?"',
+                choices: ['No', 'Yes'],
+                actions: ['breakroom.dia4', 'breakroom.dia5'],
+            });
+            break;
+            case 'dia4':  
+               newState = Object.assign(newState, {
+                dialog: '[Benny] -"That\'s what they all say. He has to be somewhere. Aww. Tell me if you find him!"',
+                choices: ['Will do Benny!', ''],
+                actions: ['breakroom.dia6', ''],
+            });
+            break;
+            case 'dia5':  
+               newState = Object.assign(newState, {
+                dialog: '[Benny] -""',
+                choices: ['', ''],
+                actions: ['', ''],
+            });
+            break;
+            case 'dia6':  
+               newState = Object.assign(newState, {
+                dialog: '[Benny] -"Thanks Mister! Good luck with whatever you\'re doing."',
+                choices: ['Exit into the hallway', ''],
+                actions: ['', ''],
+            });
+            break;
+            case 'dia':  
+               newState = Object.assign(newState, {
+                dialog: '[Benny] -""',
                 choices: ['', ''],
                 actions: ['', ''],
             });
--- Murder Mystery/javascript/scenes/hallway.js
+++ Murder Mystery/javascript/scenes/hallway.js
@@ -5,31 +5,31 @@ function hallwayActions(action, state) {
                newState = Object.assign(newState, {
                 dialog: 'You can see 2 offices on either side of you. The hallway continues forward in front of you.',
                 choices: ['John\'s office is on your left. It is empty.', 'Steve\'s office is to your left. You can hear a quiet sobbing from inside.', 'The hallway ahead of you leads to more rooms.'],
-                actions: ['', '', ''],
+                actions: ['johnoffice.ent', 'steveoffice.ent', 'hallway.ent2'],
             });
             break;
-            case 'confent2':  
+            case 'ent2':  
                newState = Object.assign(newState, {
                 dialog: 'You can see 2 rooms on either side of you. The hallway continues forward in front of you.',
                 choices: ['A storage room can be seen to your left.', 'Meg\'s office can be seen to your right', 'The hallway makes a sharp turn ahead.'],
-                actions: ['', '', ''],
+                actions: ['storageroom.ent', 'megoffice.ent', 'hallway.ent3'],
             });
             break;
-            case 'confent3':  
+            case 'ent3':  
                newState = Object.assign(newState, {
                 dialog: 'You can see 2 rooms on either side of you. The hallway continues forward in front of you.',
                 choices: ['What appears to be a breakroom can be seen to your left.', 'A copier room is stationed to your right.', 'The hallway continues onward ahead.'],
-                actions: ['', '', ''],
+                actions: ['breakroom.ent', 'copierroom.ent', 'hallway.ent4'],
             });
             break;
-            case 'confent4':  
+            case 'ent4':  
                newState = Object.assign(newState, {
                 dialog: 'You can see 2 offices on either side of you. The hallway continues forward in front of you.',
-                choices: ['Dave\'s office seems to make up the only room in this corner of the office', '', ''],
-                actions: ['', '', ''],
+                choices: ['Dave\'s office seems to make up the only room in this corner of the office', 'The Hallway seems to loop around at this point', ''],
+                actions: ['daveoffice.ent', '', 'hallway.confent'],
             });
             break;
-            case 'confent5':  
+            case 'ent5':  
                newState = Object.assign(newState, {
                 dialog: 'You can see 2 offices on either side of you. The hallway continues forward in front of you.',
                 choices: ['', '', ''],
