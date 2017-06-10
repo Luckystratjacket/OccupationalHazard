d6ec000091bb7635487b932810dc0f7e4041666a
TheFunkyOwl <orangeade1999@gmail.com>
2017-06-09 20:18:57 -0400

    Added alexandria to john office.

--- Murder Mystery/javascript/scenes/johnOffice.js
+++ Murder Mystery/javascript/scenes/johnOffice.js
@@ -6,6 +6,7 @@ function johnofficeActions(action, state) {
                 dialog: 'You enter John\'s office. You can see a figure near his desk, sorting through things.',
                 choices: ['Hey! Who is that?', 'Oh hi.'],
                 actions: ['johnoffice.dia', 'johnoffice.dia'],
+                background: [{ url: './img/johnOfficeNew.png', width: '100%', height: '100%' }],
             });
             break;
         case 'dia':
@@ -13,6 +14,7 @@ function johnofficeActions(action, state) {
                 dialog: '[Alexandria Epicron]-"Ahh! I didn\'t see you over there! I was just looking around the room to see if I could find something of mine. Nevermind."',
                 choices: ['Find anything interesting?', 'You seem far more sane than when we last spoke.'],
                 actions: ['johnoffice.dia2', ''],
+                background: [{ url: './img/johnOffice_ALEXANDRIA_NORMAL.png', width: '100%', height: '100%' }],
             });
             break;
         case 'dia2':
@@ -20,6 +22,7 @@ function johnofficeActions(action, state) {
                 dialog: '[Alexandria Epicron]-"Well, actually, I did. I found this check made out to John. It\'s from Dave. Labelled Shush Money. Dave\'s such a jokester. Probably just a bonus."',
                 choices: ['Has it been cashed?', 'You don\'t seem that enthusiastic. What were you looking for here?'],
                 actions: ['johnoffice.dia3', 'johnoffice.dia4'],
+                background: [{ url: './img/johnOffice_ALEXANDRIA_NORMAL.png', width: '100%', height: '100%' }],
             });
             break;
         case 'dia3':
@@ -27,6 +30,7 @@ function johnofficeActions(action, state) {
                 dialog: '[Alexandria Epicron]-"Doesn\'t look like it. Have you seen anything that looks suspicious around here? I know thats usually your job, but I just need to get some sort of idea on if I can get Benny. His grandparents are monsters and I don\'t want him entering the foster system either!"',
                 choices: ['Nope', 'What\'s that red stain on the ground?'],
                 actions: ['johnoffice.dia5', 'johnoffice.dia6'],
+                background: [{ url: './img/johnOffice_ALEXANDRIA_ANGRY.png', width: '100%', height: '100%' }],
             });
             break;
         case 'dia4':
@@ -34,6 +38,7 @@ function johnofficeActions(action, state) {
                 dialog: '[Alexandria Epicron]-"Have you seen anything that looks suspicious around here? I know thats usually your job, but I just need to get some sort of idea on if I can get Benny. His grandparents are monsters and I don\'t want him entering the foster system either!"',
                 choices: ['No, Sorry.', 'What\'s that red stain on the ground?'],
                 actions: ['', ''],
+                background: [{ url: './img/johnOffice_ALEXANDRIA_ANGRY.png', width: '100%', height: '100%' }],
             });
             break;
         case 'dia5':
@@ -41,6 +46,7 @@ function johnofficeActions(action, state) {
                 dialog: '[Alexandria Epicron]-"Oh. Well that sucks! If you find anything lying around, just let me know."',
                 choices: ['Ok'],
                 actions: ['hallway.confent'],
+                background: [{ url: './img/johnOffice_ALEXANDRIA_NORMAL.png', width: '100%', height: '100%' }],
             });
             break;
         case 'dia6':
@@ -48,6 +54,7 @@ function johnofficeActions(action, state) {
                 dialog: '[Alexandria Epicron]-"Oh that? I accidentially dropped a glass of wine on the ground when I came back in. The janitor should come and clean that up."',
                 choices: ['I havent actually seen a janitor anywhere', ''],
                 actions: ['johnoffice.dia5', ''],
+                background: [{ url: './img/johnOffice_ALEXANDRIA_NORMAL.png', width: '100%', height: '100%' }],
             });
             break;
     }
