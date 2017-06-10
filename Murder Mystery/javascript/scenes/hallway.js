f87995bd1ba457373d1a0b5aa8ab5002ceb10d60
TheFunkyOwl <orangeade1999@gmail.com>
2017-06-09 20:31:38 -0400

    .

--- Murder Mystery/javascript/scenes/hallway.js
+++ Murder Mystery/javascript/scenes/hallway.js
@@ -14,6 +14,7 @@ function hallwayActions(action, state) {
                 dialog: 'You can see 2 rooms on either side of you. The hallway continues forward in front of you.',
                 choices: ['A storage room can be seen to your left.', 'Meg\'s office can be seen to your right', 'The hallway makes a sharp turn ahead.'],
                 actions: ['storageroom.ent', 'megoffice.ent', 'hallway.ent3'],
+                background: [{ url: './img/hallway_2.png', width: '100%', height: '100%' }],
             });
             break;
             case 'ent3':  
@@ -21,6 +22,7 @@ function hallwayActions(action, state) {
                 dialog: 'You can see 2 rooms on either side of you. The hallway continues forward in front of you.',
                 choices: ['What appears to be a breakroom can be seen to your left.', 'A copier room is stationed to your right.', 'The hallway continues onward ahead.'],
                 actions: ['breakroom.ent', 'copierroom.ent', 'hallway.ent4'],
+                background: [{ url: './img/hallway_1.png', width: '100%', height: '100%' }],
             });
             break;
             case 'ent4':  
@@ -28,6 +30,7 @@ function hallwayActions(action, state) {
                 dialog: 'You can see 2 offices on either side of you. The hallway continues forward in front of you.',
                 choices: ['Daves Office is visible on the left', 'The Hallway continues around here', 'A large oak door secured with a hefty lock is visible to the right.'],
                 actions: ['daveoffice.ent', 'hallway.ent5', 'hallway.ent6'],
+                background: [{ url: './img/hallway_1.png', width: '100%', height: '100%' }],
             });
             break;
             case 'ent5':  
@@ -35,6 +38,7 @@ function hallwayActions(action, state) {
                 dialog: 'It appears to be an exit door. Only go through this door when you are done with all your reasearch in the office. The Hallway continues in front of you',
                 choices: ['The big oak doors appear to lead to the outside.', 'The hallway makes a sharp turn ahead.', ''],
                 actions: ['finale.door', 'hallway.confent', ''],
+                background: [{ url: './img/door_no_key.png', width: '100%', height: '100%' }],
             });
             break;
             case 'ent6':
